1cefb0b0f01f1a0c92aa47f22b6b8465eceb692b
Liz <Liz@DESKTOP-2088845>
2018-09-29 17:33:08 +0200

    all

---
+++ target/generated-sources/it/unibo/test/services/ws/AddCredentialsResponse.java
@@ -0,0 +1,62 @@
+
+package it.unibo.test.services.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for addCredentialsResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="addCredentialsResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://services.unibo.it/}userBean" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "addCredentialsResponse", propOrder = {
+    "_return"
+})
+public class AddCredentialsResponse {
+
+    @XmlElement(name = "return")
+    protected UserBean _return;
+
+    /**
+     * Gets the value of the return property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link UserBean }
+     *     
+     */
+    public UserBean getReturn() {
+        return _return;
+    }
+
+    /**
+     * Sets the value of the return property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link UserBean }
+     *     
+     */
+    public void setReturn(UserBean value) {
+        this._return = value;
+    }
+
+}
---
+++ target/generated-sources/it/unibo/test/services/ws/GetCredential.java
@@ -0,0 +1,60 @@
+
+package it.unibo.test.services.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getCredential complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getCredential">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="idUser" type="{http://www.w3.org/2001/XMLSchema}int" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getCredential", propOrder = {
+    "idUser"
+})
+public class GetCredential {
+
+    protected Integer idUser;
+
+    /**
+     * Gets the value of the idUser property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    public Integer getIdUser() {
+        return idUser;
+    }
+
+    /**
+     * Sets the value of the idUser property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    public void setIdUser(Integer value) {
+        this.idUser = value;
+    }
+
+}
